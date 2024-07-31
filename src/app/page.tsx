0ef126f1a9203a85e7b6b699dc862e5849d05814
Jenish <jpat534@gmail.com>
2024-07-31 17:22:16 +1000

    Updated the input field to a form.
    The form should allow you to select a priority from a list of options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import List from "@/components/list";
 import Input from "../components/input";
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     <main>
       <h1 className="flex justify-center p-4 m-4 text-5xl">To-do List</h1>
       <Input />
-      {/* Table of Todos */}
+      <List />
       {/* Table of completed Todos */}
     </main>
   );
