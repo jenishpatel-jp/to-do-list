92965a5995761561d0d5bd626bdebd2a40a1572e
Jenish <jpat534@gmail.com>
2024-08-01 10:04:08 +1000

    Added more customisation to the input form

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,24 +1,47 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from './ui/button'
 
 const Input = () => {
 
-  const handleClick = () => {
-    console.log("Add button has been pressed.");
+  const [description, setDescription] = useState<string>('');
+  const [priority, setPriority] = useState<string>('')
+
+  const handleSubmit = (e:React.FormEvent) => {
+    e.preventDefault();
   }
 
   return (
-    <div className='flex justify-center p-2 m-2 flex-1 border border-white items-center'>
-        <form>
-          <label>
-            <input className='rounded-md m-3 p-3 w-1/3 text-center border border-white text-black' placeholder='Add things you need to do...' />
-          </label>
-          <Button className='bg-green-400 m-3 p-3 border border-white' onClick={() => handleClick()}>Add</Button>
-        </form>
-    </div>
+    <form onSubmit={handleSubmit} className='border border-white p-2 m-2 flex justify-center'>
+      <div className='flex items-center p-2 w-3/4'>
+        <label htmlFor='description' className='p-2'>Description:</label>
+        <input
+          type='text'
+          id='description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+          className='text-black w-full border border-white'
+        />
+      </div>
+      <div className='flex items-center space-x-2'>
+        <label htmlFor='priority' className='p-2'>Priority:</label>
+        <select
+          id='priority'
+          value={priority}
+          onChange={e => setPriority(e.target.value)}
+          className='text-black p-1 m-2 rounded-md'
+        >
+          <option>High</option>
+          <option>Medium</option>
+          <option>Low</option>
+
+        </select>
+      </div>
+      <Button type='submit' className='m-2 p-2 bg-green-400 hover:bg-green-700' >Add Todo</Button>
+    </form>
   )
 }
 
-export default Input
+export default Input;
