4eac90f87526d12f8c7da135d96c7476162136cf
Jenish <jpat534@gmail.com>
2024-08-02 14:05:21 +1000

    Updated the Todo Schema to include userID.

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,6 +1,10 @@
 import { Schema, model, models } from 'mongoose';
 
 const todoSchema = new Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
     description: {
         type: String,
         required: true,
