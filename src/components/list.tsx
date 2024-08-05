96604ebec80561fe7b88397c4d35f30e58ce544e
Jenish <jpat534@gmail.com>
2024-08-05 18:00:36 +1000

    Added a GET fetch request for todos.

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+interface Todos {
+  _id: number;
+  userId: string;
+  descrpition: string;
+  priority: string, 
+  completed: boolean,
+}
 
 const List = () => {
+
+  const [todos, setTodos] = useState<Todos | []> ([]);
+
+  const getTodos = async () => {
+    const response = await fetch('/api/todos');
+    if(!response.ok){
+      throw new Error ('Failed to fetch todos');
+    } 
+    const json = await response.json();
+    console.log(json);
+    setTodos(json);
+  }
+
+  getTodos();
+
   return (
     <div className='justify-center'>
 
