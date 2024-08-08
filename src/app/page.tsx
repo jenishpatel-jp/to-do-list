417f19acad0c05af3e324caab8e8eef8d84943c7
Jenish <jpat534@gmail.com>
2024-08-08 10:55:07 +1000

    Added functionality to reverse the completed todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,6 +31,7 @@ export default function Home() {
       />
       <Completed 
         todos={todos}
+        setTodos={setTodos}
       />
     </main>
   );
--- src/components/completed.tsx
+++ src/components/completed.tsx
@@ -1,19 +1,26 @@
 import React, { useEffect } from 'react'
-import { Todos } from '@/utils/listUtils'
+import { Todos, handleStatusChange } from '@/utils/listUtils'
 import { RotateCcw, Trash } from 'lucide-react';
 
 interface CompletedTodos{
     todos: Todos[];
+    setTodos: React.Dispatch<React.SetStateAction<Todos[]>>
 };
 
-const Completed: React.FC<CompletedTodos> = ( {todos} ) => {
+const Completed: React.FC<CompletedTodos> = ( {todos, setTodos} ) => {
 
     const listCompleted = todos.map(todo => todo.completed ? (
     <div key={todo._id} className='bg-green-500 flex p-2 m-2 items-center justify-between'>
         <p className='flex' >{ todo.description }</p>
         <div className='flex w-16 h-9 items-center justify-between mr-5'>
-            <RotateCcw className=''/>
-            <Trash className=''/>
+            <button
+                onClick={()=> handleStatusChange(todo._id, !todo.completed, setTodos, todos)}
+            >
+                <RotateCcw className=''/>
+            </button>
+            <button>
+                <Trash className=''/>
+            </button>
         </div>
     </div>
     ):
